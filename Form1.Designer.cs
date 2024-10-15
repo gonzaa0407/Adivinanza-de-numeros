ab1b4589dfa267c5505942de7e155983d753d542
gonza <gonza@DESKTOP-CM79C0T>
2024-10-15 19:50:14 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Adivinanza_de_numeros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdivinar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(131, 123);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(162, 20);
+            this.txtNumero.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "*INGRESE UN NUMERO*";
+            // 
+            // btnAdivinar
+            // 
+            this.btnAdivinar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAdivinar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdivinar.Location = new System.Drawing.Point(131, 167);
+            this.btnAdivinar.Name = "btnAdivinar";
+            this.btnAdivinar.Size = new System.Drawing.Size(167, 51);
+            this.btnAdivinar.TabIndex = 2;
+            this.btnAdivinar.Text = "Adivinar Numero";
+            this.btnAdivinar.UseVisualStyleBackColor = false;
+            this.btnAdivinar.Click += new System.EventHandler(this.btnAdivinar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblResultado.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(367, 130);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 15);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnReiniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReiniciar.Location = new System.Drawing.Point(326, 167);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(167, 51);
+            this.btnReiniciar.TabIndex = 4;
+            this.btnReiniciar.Text = "Intentar nuevamente";
+            this.btnReiniciar.UseVisualStyleBackColor = false;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(209, 54);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(374, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "JUEGO DE ADIVINANZA DE NUMEROS";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnAdivinar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdivinar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Adivinanza_de_numeros
+{
+    public partial class Form1 : Form
+    {
+        int numerosecreto;
+        int intentos;
+        public Form1()
+        {
+            InitializeComponent();
+            ReiniciarJuego();
+        }
+
+        private void btnAdivinar_Click(object sender, EventArgs e)
+        {
+            // Comprobamos si el usuario ingreso un número válido
+            int numeroUsuario;
+            if (int.TryParse(txtNumero.Text, out numeroUsuario))
+            {
+                intentos++;
+
+                // Comparar el número del usuario con el número secreto
+                if (numeroUsuario == numerosecreto)
+                {
+                    lblResultado.Text = $"¡Correcto! Adivinaste el número en {intentos} intentos.";
+                    btnAdivinar.Enabled = false;  // Desactivar el botón para no seguir jugando
+                }
+                else if (numeroUsuario < numerosecreto)
+                {
+                    lblResultado.Text = "El número es mayor.";
+                }
+                else
+                {
+                    lblResultado.Text = "El número es menor.";
+                }
+            }
+            else
+            {
+                lblResultado.Text = "Por favor, ingresa un número válido.";
+            }
+
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            ReiniciarJuego();
+        }
+        private void ReiniciarJuego()
+        {
+            // Genera un número aleatorio del 1 y 100
+            Random random = new Random();
+            numerosecreto = random.Next(1, 101);
+            intentos = 0;
+
+            lblResultado.Text = "Adivina un número entre 1 y 100.";
+            txtNumero.Text = "";
+            btnAdivinar.Enabled = true;  // Se vuelve a habilitar el botón de adivinanza
+        }
+
+    }
+}
